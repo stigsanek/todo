2849eaec33fd711f6de0d3c0abd1615c10795d04
stigsanek <stigsanek@yandex.ru>
2020-07-30 16:45:30 +0400

    Добавляет компоненты и стили для них

---
+++ src/components/app/app.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import AppHeader from '../app-header/';
+import SearchPanel from '../search-panel/';
+import ItemStatusFilter from '../item-status-filter/';
+import TodoList from '../todo-list/';
+import './app.css';
+
+const App = () => {
+  const todoData = [
+    { id: 1, label: 'Drink Coffee', important: false },
+    { id: 2, label: 'Make Awesome App', important: true },
+    { id: 3, label: 'Build React App', important: true }
+  ];
+
+  return (
+    <div className="todo-app">
+      <AppHeader toDo={1} done={3} />
+      <div className="top-panel d-flex">
+        <SearchPanel />
+        <ItemStatusFilter />
+      </div>
+      <TodoList todos={todoData} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/todo-list-item/todo-list-item.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import './todo-list-item.css';
+
+const TodoListItem = ({ label, important = false }) => {
+  const style = {
+    color: important ? 'steelblue' : 'black',
+    fontWeight: important ? 'bold' : 'normal'
+  }
+
+  return (
+    <div className="todo-list-item">
+      <span style={style} className="todo-list-item-label">
+        {label}
+      </span>
+
+      <button type="button" className="btn btn-outline-success btn-sm float-right">
+        <i className="fa fa-exclamation" />
+      </button>
+      <button type="button" className="btn btn-outline-danger btn-sm float-right">
+        <i className="fa fa-trash-o" />
+      </button>
+    </div >
+  );
+}
+
+export default TodoListItem;
