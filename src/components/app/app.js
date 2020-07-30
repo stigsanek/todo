75b3da7285577dda0f0a326ede017238d7ce6882
stigsanek <stigsanek@yandex.ru>
2020-07-30 23:04:22 +0400

    Преобразует компонент в классовый, добавляет логику удаления элемента списка

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import AppHeader from '../app-header/';
 import SearchPanel from '../search-panel/';
@@ -6,28 +6,47 @@ import ItemStatusFilter from '../item-status-filter/';
 import TodoList from '../todo-list/';
 import './app.css';
 
-const App = () => {
-  const todoData = [
-    { id: 1, label: 'Drink Coffee', important: false },
-    { id: 2, label: 'Make Awesome App', important: true },
-    { id: 3, label: 'Build React App', important: true }
-  ];
+export default class App extends Component {
+  state = {
+    todoData: [
+      { id: 1, label: 'Drink Coffee', important: false },
+      { id: 2, label: 'Make Awesome App', important: true },
+      { id: 3, label: 'Build React App', important: true }
+    ]
+  }
 
-  return (
-    <div className="todo-app">
-      <AppHeader toDo={1} done={3} />
+  deleteItem = (id) => {
+    this.setState(({ todoData }) => {
+      const index = todoData.findIndex((element) => {
+        return element.id === id;
+      });
 
-      <div className="top-panel d-flex">
-        <SearchPanel />
-        <ItemStatusFilter />
+      const newArray = [
+        ...todoData.slice(0, index),
+        ...todoData.slice(index + 1)
+      ];
+
+      return {
+        todoData: newArray
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div className="todo-app">
+        <AppHeader toDo={1} done={3} />
+
+        <div className="top-panel d-flex">
+          <SearchPanel />
+          <ItemStatusFilter />
+        </div>
+
+        <TodoList
+          todos={this.state.todoData}
+          onDeleted={this.deleteItem}
+        />
       </div>
-
-      <TodoList
-        todos={todoData}
-        onDeleted={(id) => { console.log(id) }}
-      />
-    </div>
-  );
+    );
+  }
 }
-
-export default App;
