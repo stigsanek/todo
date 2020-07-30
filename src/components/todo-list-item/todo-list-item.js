f92e8f56b026afb82c880365b3e72e607c3e1be7
stigsanek <stigsanek@yandex.ru>
2020-07-30 18:22:15 +0400

    Добавляет событие для удаления элементов из списка

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -25,7 +25,7 @@ export default class TodoListItem extends Component {
   }
 
   render() {
-    const { label } = this.props;
+    const { label, onDeletedItem } = this.props;
     const { done, important } = this.state;
 
     let classNames = 'todo-list-item';
@@ -43,13 +43,18 @@ export default class TodoListItem extends Component {
           {label}
         </span>
 
-        <button type="button"
+        <button
+          type="button"
           className="btn btn-outline-success btn-sm float-right"
           onClick={this.onMarkClick}
         >
           <i className="fa fa-exclamation" />
         </button>
-        <button type="button" className="btn btn-outline-danger btn-sm float-right">
+        <button
+          type="button"
+          className="btn btn-outline-danger btn-sm float-right"
+          onClick={onDeletedItem}
+        >
           <i className="fa fa-trash-o" />
         </button>
       </div >
--- src/components/todo-list/todo-list.js
+++ src/components/todo-list/todo-list.js
@@ -3,14 +3,17 @@ import React from 'react';
 import TodoListItem from '../todo-list-item/';
 import './todo-list.css';
 
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, onDeleted }) => {
   const elements = todos.map((item) => {
     const { id, ...itemProps } = item;
 
     return (
       // Use spread syntax for object
       <li key={id} className="list-group-item">
-        <TodoListItem {...itemProps } />
+        <TodoListItem
+          {...itemProps}
+          onDeletedItem={() => { onDeleted(id) }}
+        />
       </li>
     );
   })
